7df591f3a95ca44e24674f2f36f58a3c02e5820b
Hazukashio <hazukashio@gmail.com>
2020-10-17 22:08:50 +0700

    Update kernel-hmp.sh

--- kernel-hmp.sh
+++ kernel-hmp.sh
@@ -60,7 +60,7 @@ PTTG=1
 	if [ $PTTG = 1 ]
 	then
 		# Set Telegram Chat ID
-		CHATID="-1001186903305"
+		CHATID="-1217787640"
 	fi
 
 # Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
