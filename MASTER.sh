c277d8cb8aef90c42a1e6d164c223b60847a5117
Hikari <73871195+Takanashi-Hikari@users.noreply.github.com>
2020-11-19 23:35:37 +0700

    Update MASTER.sh

--- MASTER.sh
+++ MASTER.sh
@@ -101,13 +101,13 @@ then
 	if [ -n "$CIRCLECI" ]
 	then
 		export KBUILD_BUILD_VERSION=$CIRCLE_BUILD_NUM
-		export KBUILD_BUILD_HOST="@TeaXLatte"
+		export KBUILD_BUILD_HOST="TeaXLatte"
 		export CI_BRANCH=$CIRCLE_BRANCH
 	fi
 	if [ -n "$DRONE" ]
 	then
 		export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
-		export KBUILD_BUILD_HOST=@TeaXLatte
+		export KBUILD_BUILD_HOST=TeaXLatte
 		export CI_BRANCH=$DRONE_BRANCH
 	else
 		echo "Not presetting Build Version"
