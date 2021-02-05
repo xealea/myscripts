d6c779196b73a65c50a48dbab5241ea2bfa1e185
ビクセル <vcyzscape@gmail.com>
2021-02-05 21:25:51 +0700

    Update MASTER.sh

--- MASTER.sh
+++ MASTER.sh
@@ -90,7 +90,7 @@ LOG_DEBUG=0
 
 ## Set defaults first
 DISTRO=$(cat /etc/issue)
-KBUILD_BUILD_HOST=DeathSenpai
+KBUILD_BUILD_HOST=I O F I
 CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 token=$TELEGRAM_TOKEN
 export KBUILD_BUILD_HOST CI_BRANCH
@@ -101,13 +101,13 @@ then
 	if [ -n "$CIRCLECI" ]
 	then
 		export KBUILD_BUILD_VERSION=$CIRCLE_BUILD_NUM
-		export KBUILD_BUILD_HOST="Calliope"
+		export KBUILD_BUILD_HOST="M Y - W I F R"
 		export CI_BRANCH=$CIRCLE_BRANCH
 	fi
 	if [ -n "$DRONE" ]
 	then
 		export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
-		export KBUILD_BUILD_HOST=Calliope
+		export KBUILD_BUILD_HOST=M Y - W I F E
 		export CI_BRANCH=$DRONE_BRANCH
 	else
 		echo "Not presetting Build Version"
@@ -143,7 +143,7 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 ##------------------------------------------------------##
 
 exports() {
-	export KBUILD_BUILD_USER="DeathSenpai"
+	export KBUILD_BUILD_USER="I O F I"
 	export ARCH=arm64
 	export SUBARCH=arm64
         
