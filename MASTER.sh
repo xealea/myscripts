5e34a4a71c89f117ce4bd0eb74cb2e41a9e4794e
ビクセル <vcyzscape@gmail.com>
2021-02-08 13:20:03 +0700

    Update MASTER.sh

--- MASTER.sh
+++ MASTER.sh
@@ -90,7 +90,7 @@ LOG_DEBUG=0
 
 ## Set defaults first
 DISTRO=$(cat /etc/issue)
-KBUILD_BUILD_HOST=IOFI
+KBUILD_BUILD_HOST=Archlinux
 CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 token=$TELEGRAM_TOKEN
 export KBUILD_BUILD_HOST CI_BRANCH
@@ -101,13 +101,13 @@ then
 	if [ -n "$CIRCLECI" ]
 	then
 		export KBUILD_BUILD_VERSION=$CIRCLE_BUILD_NUM
-		export KBUILD_BUILD_HOST="AI"
+		export KBUILD_BUILD_HOST="Vcyzteen"
 		export CI_BRANCH=$CIRCLE_BRANCH
 	fi
 	if [ -n "$DRONE" ]
 	then
 		export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
-		export KBUILD_BUILD_HOST=AI
+		export KBUILD_BUILD_HOST=Vcyzteen
 		export CI_BRANCH=$DRONE_BRANCH
 	else
 		echo "Not presetting Build Version"
@@ -143,7 +143,7 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 ##------------------------------------------------------##
 
 exports() {
-	export KBUILD_BUILD_USER="IOFI"
+	export KBUILD_BUILD_USER="Archlinux"
 	export ARCH=arm64
 	export SUBARCH=arm64
         
