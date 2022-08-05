0f0a6567707a616fc1d1450b4ee0f4e9e3b9d66f
ʏᴜᴜ <vcyzteen@protonmail.com>
2022-08-06 01:17:15 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -182,9 +182,9 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 	echo " "
 	if [ $COMPILER = "gcc" ]
 	then
-		msg "|| Cloning GCC 9.3.0 baremetal ||"
-		git clone --depth=1 https://$GIT@github.com/vcyzteen/toolchain-private -b 64 gcc64
-		git clone --depth=1 https://$GIT@github.com/vcyzteen/toolchain-private -b 32 gcc32
+		msg "|| Cloning GCC 4.9 baremetal ||"
+		git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9.git gcc64
+		git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9.git gcc32
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
 	fi
@@ -219,7 +219,7 @@ exports() {
 		PATH=$TC_DIR/bin/:$PATH
 	elif [ $COMPILER = "gcc" ]
 	then
-		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-linux-android-gcc  --version | head -n 1)
+		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-linux-android-gcc --version | head -n 1)
 		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 	fi
 
@@ -297,14 +297,14 @@ build_kernel() {
 	elif [ $COMPILER = "gcc" ]
 	then
 		MAKE+=(
-			CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-			CROSS_COMPILE=aarch64-linux-gnu- \
-			AR=aarch64-linux-gnu-ar \
-			OBJDUMP=aarch64-linux-gnu-objdump \
-			STRIP=aarch64-linux-gnu-strip \
-			NM=aarch64-linux-gnu-nm \
-			OBJCOPY=aarch64-linux-gnu-objcopy \
-			LD=aarch64-linux-gnu-$LINKER
+			CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+			CROSS_COMPILE=aarch64-linux-android- \
+			AR=aarch64-linux-android-ar \
+			OBJDUMP=aarch64-linux-android-objdump \
+			STRIP=aarch64-linux-android-strip \
+			NM=aarch64-linux-android-nm \
+			OBJCOPY=aarch64-linux-android-objcopy \
+			LD=aarch64-linux-android-$LINKER
 		)
 	fi
 	
