d9b6d539989abd1379451c5d71190ee1128d9b93
ʟᴇᴀ <vcyzteen@protonmail.com>
2022-08-25 23:28:40 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -183,8 +183,8 @@ clone() {
 	if [ $COMPILER = "gcc" ]
 	then
 		msg "|| Cloning GCC xea-xo1 12.2.1 Baremetal ||"
-		git clone --depth=1 https://github.com/xealea/toolchain-priv -b arm64 gcc64
-                git clone --depth=1 https://github.com/xealea/toolchain-priv -b arm gcc32
+		git clone --depth=1 https://$GITHUB_TOKEN@github.com/xealea/toolchain-priv -b arm64 gcc64
+                git clone --depth=1 https://$GITHUB_TOKEN@github.com/xealea/toolchain-priv -b arm gcc32
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
 	fi
